ad47d0f45207dbeb7eecfe512c7d81e5b89c9d09
joaogabrielmp <joaogabrielma@gmail.com>
2020-07-20 14:35:26 -0300

    up

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb",
+        "plugin:prettier/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "prettier"
+    ],
+    "rules": {
+      "prettier/prettier": "error",
+      "react/jsx-filename-extension": [
+        "warn",
+        {
+          "extensions": [".jsx", ".js"]
+        }
+      ],
+      "import/prefer-default-export": "off",
+      "no-param-reassign": "off",
+      "react/jsx-one-expression-per-line": "off",
+      "react/jsx-props-no-spreading": "off",
+      "react/forbid-prop-types": "off"
+    }
+};
