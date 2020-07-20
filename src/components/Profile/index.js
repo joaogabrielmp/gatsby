9c0589268ce1ec71b4043e52f832da1ef821561e
joaogabrielmp <joaogabrielma@gmail.com>
2020-07-20 18:36:21 -0300

    up

---
+++ src/components/Profile/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>John Doe</h1>
+    <h2>Programador Backend</h2>
+    <p>
+      Membro do Core Team da Empresa Foo, escrevo sobre o mundo backend e outras
+      coisas mais.
+    </p>
+  </div>
+)
+
+export default Profile
