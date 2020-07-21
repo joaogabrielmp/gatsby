b36e71926a3344a1e66dae917972796f21075190
joaogabrielmp <joaogabrielma@gmail.com>
2020-07-21 20:13:20 -0300

    up

--- src/utils/getThemeColor.js
+++ src/utils/getThemeColor.js
@@ -1,8 +1,11 @@
 const getThemeColor = () => {
   const theme = typeof window !== 'undefined' && window.__theme
 
-  if (theme === 'light') return '#fff'
-  if (theme === 'dark') return '#16202c'
+  if (theme === 'light') {
+    return '#fff'
+  }
+
+  return '#16202c'
 }
 
 export default getThemeColor
